af61ff55655d78c744bd283e1b6f73bdc0ccee78
Haider Rizvi <rizvihaider0801@icloud.com>
2026-03-03 00:05:37 +0530

    New changes

--- main.py
+++ main.py
@@ -62,11 +62,11 @@ def main(page: ft.Page) -> None:
 
 if __name__ == "__main__":
     os.environ["FLET_SERVER_PORT"] = "8080"
-    # os.environ["FLET_SERVER_IP"] = "0.0.0.0"
+    os.environ["FLET_SERVER_IP"] = "0.0.0.0"
 
     ft.run(
         main=main,
         view=ft.AppView.WEB_BROWSER,
-        port=8080 #,                   
-        # host="0.0.0.0"                
+        port=8080,                   
+        host="0.0.0.0"                
     )
